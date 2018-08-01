e91f3450ead850b7a5cebf0d8390f56676acc31d
hgrigor98 <hgrigor93@gmail.com>
2018-08-01 11:01:50 +0400

    Added future 1

--- ConsoleApp/ConsoleApp/Future1.cs
+++ ConsoleApp/ConsoleApp/Future1.cs
@@ -6,11 +6,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
-    class Program
+    class Future1
     {
-        static void Main(string[] args)
-        {
-            //Code to call future 1
-        }
     }
 }
